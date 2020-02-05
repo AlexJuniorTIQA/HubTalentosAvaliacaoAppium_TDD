d4e0ff123ebcdd8e2359c5ba47b717d8ad2b9525
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-02-05 11:11:27 -0300

    Mapeado novos elementos e seus métodos.

--- HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/pages/HomePage.java
+++ HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/pages/HomePage.java
@@ -1,55 +1,53 @@
 package br.com.rsinet.appium.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Classe de mapeamento de elementos: HomePage do app AdvantageOnlineShopping
  **/
 public class HomePage {
 
+	
 	final WebDriver driver;
 	
+	
 	@FindBy(how = How.ID, using = "com.Advantage.aShopping:id/imageViewMenu")
-
-	public WebElement elementMenu;
+	private WebElement elementMenu;
+	
 	@FindBy(how = How.ID, using = "com.Advantage.aShopping:id/textViewMenuUser")
-
-	public WebElement elementIconUser;
+	private WebElement elementIconUser;
 
 	@FindBy(how = How.ID, using = "")
-
 	private WebElement elementIconSearch;
 
 	@FindBy(how = How.ID, using = "")
-
 	private WebElement elementSearch;
 
 	@FindBy(how = How.ID, using = "com.Advantage.aShopping:id/textViewDontHaveAnAccount")
-
 	private WebElement elementCreatNewAccount;
 
 	@FindBy(how = How.ID, using = "")
-
 	private WebElement elementTablets;
 
 	@FindBy(how = How.ID, using = "")
-
 	private WebElement elementHeadPhones;
 
 	@FindBy(how = How.ID, using = "")
-
 	private WebElement elementSpeakers;
 
 	@FindBy(how = How.ID, using = "")
-
 	private WebElement elementLaptops;
 
 	@FindBy(how = How.ID, using = "")
-
 	private WebElement elementMice;
 
 	@FindBy(how = How.XPATH, using = "")
@@ -59,8 +57,10 @@ public class HomePage {
 
 		this.driver = driver;
 	}
+	
 	/** Retorna um click ao Icone Menu do app **/
 	public void clickMenu() {
+		
 		elementMenu.click();
 	}
 	/** Retorna um click ao Icone de Usuário **/
@@ -71,5 +71,10 @@ public class HomePage {
 	/** Retorna um click ao Icone de Criar Nova Conta **/
 	public void clickCreatNewAccount() {
 		elementCreatNewAccount.click();;
+	}
+	/** Retorna um getText ao Icone de Criar Nova Conta **/
+	public  String getIconUser() {
+		return elementIconUser.getText();
+		
 	}
 }
