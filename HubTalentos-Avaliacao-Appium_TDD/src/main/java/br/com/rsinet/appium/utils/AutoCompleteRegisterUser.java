349e4a8bec6195c731d936bcabdd082f0816f842
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-02-05 11:11:03 -0300

    Criado o método de setar as informações do usuário pelo campo "

--- HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/utils/AutoCompleteRegisterUser.java
+++ HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/utils/AutoCompleteRegisterUser.java
@@ -6,18 +6,19 @@ import org.openqa.selenium.support.PageFactory;
 import br.com.rsinet.appium.pages.RegisterPage;
 import io.appium.java_client.android.AndroidDriver;
 
-
-
 public class AutoCompleteRegisterUser {
 
-	
 	final AndroidDriver<WebElement> driver;
+
 	public AutoCompleteRegisterUser(AndroidDriver<WebElement> driver) {
 
 		this.driver = driver;
 	}
-	
-	/**Classe que preenche todos os campos da página de registro(RegisterPage): Recebe como parâmetro um objeto do tipo integer**/
+
+	/**
+	 * Classe que preenche todos os campos da página de registro(RegisterPage):
+	 * Recebe como parâmetro um objeto do tipo integer
+	 **/
 	public void setUserRegister(AndroidDriver<WebElement> driver, int numberUser) throws Exception {
 
 		RegisterPage registerPage = PageFactory.initElements(driver, RegisterPage.class);
@@ -32,14 +33,24 @@ public class AutoCompleteRegisterUser {
 		registerPage.setLastName(ExcelUtils.getCellData(numberUser, 5));
 		registerPage.setPhoneNumber(ExcelUtils.getCellData(numberUser, 6));
 
-		DriverFactory.pageRoll();
-		
+		DriverFactory.pageRollDawn();
+
 		registerPage.selectCountry(driver, ExcelUtils.getCellData(numberUser, 7));
-		
-		registerPage.setCity(ExcelUtils.getCellData(numberUser, 8));
+		registerPage.setState(ExcelUtils.getCellData(numberUser, 8));
 		registerPage.setAdress(ExcelUtils.getCellData(numberUser, 9));
-		registerPage.setState(ExcelUtils.getCellData(numberUser, 10));
+		registerPage.setCity(ExcelUtils.getCellData(numberUser, 10));
 		registerPage.setPostalCode(ExcelUtils.getCellData(numberUser, 11));
-		
+
 	}
+	public void setAccountDetailsUserRegister(AndroidDriver<WebElement> driver, int numberUser) throws Exception {
+
+		RegisterPage registerPage = PageFactory.initElements(driver, RegisterPage.class);
+		ExcelUtils.setExcelFile(Constant.File_DataUserRegister, "Users");
+
+		registerPage.setUserName(ExcelUtils.getCellData(numberUser, 1));
+		registerPage.setEmail(ExcelUtils.getCellData(numberUser, 2));
+		registerPage.setPassword(ExcelUtils.getCellData(numberUser, 3));
+		registerPage.setConfirmPassword(ExcelUtils.getCellData(numberUser, 3));
+	}
+	
 }
