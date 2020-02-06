5ef10185d1b2061421286b2ed35c031eb1875c35
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-02-06 15:41:26 -0300

    Inserção do driverWait na page.

--- HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/pages/ProductPage.java
+++ HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/pages/ProductPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Classe de mapeamento de elementos: Página de Produtos do site
@@ -13,22 +15,44 @@ import org.openqa.selenium.support.How;
 public class ProductPage {
 
 	final WebDriver driver;
-
-	@FindBy(how = How.XPATH, using = "")
+	private WebDriverWait wait;
+	
+	@FindBy(how = How.ID, using = "com.Advantage.aShopping:id/textViewProductName")
 	private WebElement elementProductName;
+	
 
-
-
-	@FindBy(how = How.XPATH, using = "/")
+	@FindBy(how = How.ID, using = "com.Advantage.aShopping:id/textViewNoProductsToShow")
 	private WebElement elementNotFound;
 
+	@FindBy(how = How.ID, using = "com.Advantage.aShopping:id/imageViewFilter")
+	private WebElement elementFilter;
 
-
+	@FindBy(how = How.ID, using = "com.Advantage.aShopping:id/frameLayoutProductColor")
+	private WebElement elementSelectColor;
+	@FindBy(how = How.XPATH, using = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout[2]")
+	private WebElement elementIconColor;
+	
 	public ProductPage(WebDriver driver) {
 
 		this.driver = driver;
 	}
 
-	
-
+	public void clickProduct() {
+		elementProductName.click();
+	}
+	public String getTextProduct() {
+		return elementProductName.getText();
+	}
+	public String getNotFoundProduct() {
+		return elementNotFound.getText();
+	}
+	public void clickFilter() {
+		elementFilter.click();
+	}
+	public void clickSelectColor() {
+		elementSelectColor.click();
+	}
+	public void clickIconColor() {
+		elementIconColor.click();
+	}
 }
