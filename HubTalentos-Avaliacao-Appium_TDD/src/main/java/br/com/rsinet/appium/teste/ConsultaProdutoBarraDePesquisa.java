7c83e5c6b9195c9944d947a557a446d937a371d4
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-02-06 15:43:12 -0300

    Teste produto via bara de pesquisa

--- HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/teste/ConsultaProdutoBarraDePesquisa.java
+++ HubTalentos-Avaliacao-Appium_TDD/src/main/java/br/com/rsinet/appium/teste/ConsultaProdutoBarraDePesquisa.java
@@ -1,23 +1,15 @@
 package br.com.rsinet.appium.teste;
 
-import java.io.IOException;
-import java.sql.Driver;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import br.com.rsinet.appium.pages.HomePage;
 import br.com.rsinet.appium.pages.ProductPage;
 import br.com.rsinet.appium.pages.RegisterPage;
-import br.com.rsinet.appium.utils.AutoCompleteRegisterUser;
 import br.com.rsinet.appium.utils.Constant;
 import br.com.rsinet.appium.utils.DriverFactory;
 import br.com.rsinet.appium.utils.ExcelUtils;
@@ -26,22 +18,21 @@ import io.appium.java_client.android.AndroidDriver;
 public class ConsultaProdutoBarraDePesquisa {
 
 	private AndroidDriver<WebElement> driver;
-	private RegisterPage registerPage;
 	private HomePage homePage;
 	private ProductPage productPage;
-	
-	
 
 	@Before
 	public void Inicializa() throws Exception {
 		driver = DriverFactory.getDriver();
-		registerPage = PageFactory.initElements(driver, RegisterPage.class);
+
 		homePage = PageFactory.initElements(driver, HomePage.class);
-		ExcelUtils.setExcelFile(Constant.File_DataUserRegister, "Mice");
+		productPage = PageFactory.initElements(driver, ProductPage.class);
+		ExcelUtils.setExcelFile(Constant.File_DataUserRegister, "Headphone");
 	}
+
 	@After
 	public void Finaliza() {
-		driver.quit();
+		 driver.quit();
 	}
 
 	@Test
@@ -49,18 +40,25 @@ public class ConsultaProdutoBarraDePesquisa {
 		homePage.clickIconSearch();
 		homePage.setSearch(ExcelUtils.getCellData(1, 1));
 		homePage.clickIconSearch();
-		
-		Assert.assertEquals(ExcelUtils.getCellData(1, 1).toUpperCase(),driver.findElement(By.id("com.Advantage.aShopping:id/textViewProductName")).getText());
+
+		DriverFactory.pageRollUp();
+		productPage.clickProduct();
+
+		Assert.assertEquals(ExcelUtils.getCellData(1, 1).toUpperCase(), productPage.getTextProduct());
 
 	}
-	
+
 	@Test
 	public void ConsultaProdutoBarraDePesquisaComFalha() throws Exception {
-		
+
 		homePage.clickIconSearch();
-		homePage.setSearch(ExcelUtils.getCellData(1, 10));
+
+		String product = ExcelUtils.getCellData(6, 1);
+		homePage.setSearch(product);
+
 		homePage.clickIconSearch();
-		
-			
+
+		Assert.assertEquals("- No results for \"" + product + "\" -", productPage.getNotFoundProduct());
+
 	}
 }
